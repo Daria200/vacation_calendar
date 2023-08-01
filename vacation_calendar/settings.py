ec93fb28e9f48034232a86607a4e3ae9d2de4679
Daria Neznaeva <darianeznaeva@gmail.com>
2023-08-01 19:43:38 +0200

    [WIP] code for login

--- vacation_calendar/settings.py
+++ vacation_calendar/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "vacation_calendar/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "vacation_calendar", "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- vacations/views.py
+++ vacations/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 # Create your views here.
 def add_event(request):
+    if request.method == "POST":
+        print(request.POST)
+        print("hi")
+        # Get form values
+        # start_date = request.POST["startdate"]
+        # enddate = request.POST["end_date"]
+        # print(start_date,enddate )
+        return redirect(request, "add_event")
     return render(request, "add_event.html")
