fdd035ddbc21d9ab72e7c59cd9c0990148c89fea
Daria Neznaeva <darianeznaeva@gmail.com>
2023-08-07 14:56:40 +0200

    request vacation days form, check the start and the end dates

--- vacations/views.py
+++ vacations/views.py
@@ -6,11 +6,7 @@ def vacation_request(request):
     if request.method == "POST":
         print(request.POST)
         print("hi")
-        # Get form values
-        # start_date = request.POST["startdate"]
-        # enddate = request.POST["end_date"]
-        # print(start_date,enddate )
-        return redirect(request, "vacation_request")
+        return redirect("vacation_request")
     return render(request, "employee_view/vacation_request.html")
 
 def transfer_days_request(request):
