824a5ad5f1bb7785daa4f67835143f1eac6470c5
Daria Neznaeva <darianeznaeva@gmail.com>
2023-08-21 16:53:42 +0200

    removing updating in bulk transfer requests for managers

--- manager/views.py
+++ manager/views.py
@@ -14,7 +14,6 @@ def vacation_requests(request):
     context = {"vacation_requests": vacation_requests}
 
     if request.method == "POST":
-        print(request.POST)
         selected_request_id = request.POST["selected_request_id"]
         action = request.POST["action"]
 
@@ -62,78 +61,58 @@ def transfer_days_requests(request):
     context = {"transfer_requests": transfer_requests}
 
     if request.method == "POST":
-        selected_request_ids = request.POST.getlist("selected_requests")
-        action = request.POST.get("action")
+        selected_request_id = request.POST["selected_request_id"]
+        action = request.POST["action"]
 
-        requests_to_update = Request.objects.filter(
-            request_type=2, request_status=1, pk__in=selected_request_ids
-        ).select_related("employee")
+        transfer_request_to_approve_or_reject = Request.objects.get(
+            pk=selected_request_id
+        )
+        employee = transfer_request_to_approve_or_reject.employee
+        start_date = transfer_request_to_approve_or_reject.start_date
+        end_date = transfer_request_to_approve_or_reject.end_date
 
-        for request in requests_to_update:
-            start_date = request.start_date
-            end_date = request.end_date
-            vacation_days_to_update_or_delete = VacationDay.objects.filter(
-                employee=request.employee, date__range=(start_date, end_date)
-            )
-
-        combined_queryset_vacation_days = VacationDay.objects.none()
-
-        for request in requests_to_update:
-            start_date = request.start_date
-            end_date = request.end_date
-            vacation_days_to_update_or_delete.append(
-                VacationDay.objects.filter(
-                    employee=request.employee, date__range=(start_date, end_date)
-                )
-            )
-
-        # Combine all the individual querysets into a single combined_queryset
-        for vacation_days_queryset in vacation_days_to_update_or_delete:
-            combined_queryset_vacation_days |= vacation_days_queryset
+        vacation_days_to_approve_or_reject = VacationDay.objects.filter(
+            employee=employee,
+            date__range=(start_date, end_date),
+        )
+        num_of_days = len(vacation_days_to_approve_or_reject)
 
         if action == "approve":
-            available_days_instances = []
-            previous_year_available_days_instances = []
-            for request in requests_to_update:
-                num_days = (end_date - start_date).days + 1
-                start_year = start_date.year
-                employee = request.employee
-                num_of_work_days = verify_days(
-                    employee, start_date, num_days, start_year
-                )
-
-                available_days_instance = AvailableDays.objects.get(
-                    employee=employee,
-                    year=start_year,
-                )
-                available_days_instances.append(available_days_instance)
-
-                # Update allotted days for the previous year
-                previous_year = start_year - 1
-                previous_year_available_days = AvailableDays.objects.get(
-                    employee=employee, year=previous_year
-                )
-                previous_year_available_days_instances.append(
-                    previous_year_available_days
-                )
-
+            start_year = start_date.year
+            print(employee)
+            available_days_instance_next_year = AvailableDays.objects.get(
+                employee=employee,
+                year=start_year,
+            )
+            available_days_this_year = AvailableDays.objects.get(
+                employee=employee, year=start_year - 1
+            )
+            # approve vacation days in db (VacationDay)
+            # update the request (Request)
+            # subtract days from allotted days this year
+            # add transferred days next year
             with transaction.atomic():
-                requests_to_update.update(request_status=2)
-                available_days_instance.transferred_days = F("transferred_days") + len(
-                    num_of_work_days
+                transfer_request_to_approve_or_reject.request_status = 2
+                transfer_request_to_approve_or_reject.save()
+                vacation_days_to_approve_or_reject.update(approved=True)
+                available_days_instance_next_year.transferred_days = (
+                    F("transferred_days") + num_of_days
                 )
-                available_days_instance.save()
-                previous_year_available_days.allotted_days = F("allotted_days") - len(
-                    num_of_work_days
+                available_days_instance_next_year.save()
+                available_days_this_year.allotted_days = (
+                    F("allotted_days") - num_of_days
                 )
-                previous_year_available_days.save()
-                vacation_days_to_update_or_delete.update(approved=True)
+                available_days_this_year.save()
+                messages.success(request, f"The request has been approved")
 
         elif action == "reject":
-            # Delete vacation days and update request status
+            # delete vacation days in db (VacationDay)
+            # set the request to rejected (Request)
             with transaction.atomic():
-                combined_queryset_vacation_days.delete()
-                requests_to_update.request_status = 3
+                transfer_request_to_approve_or_reject.request_status = 3
+                transfer_request_to_approve_or_reject.save()
+                vacation_days_to_approve_or_reject.delete()
+                messages.success(request, f"The request has been rejected")
 
         return redirect("transfer_days_requests")
     return render(request, "manager_view/transfer_days_requests.html", context)
