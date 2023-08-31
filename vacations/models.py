a6005b74f7dd3cd311886d5a3bb8d3fb2011d18d
Daria Neznaeva <darianeznaeva@gmail.com>
2023-08-31 09:27:57 +0200

    adding remote day table, adding the remotedays typ e to the request table, adding 30 remote work days

--- vacations/models.py
+++ vacations/models.py
@@ -51,6 +51,7 @@ class AvailableDays(models.Model):
     )
     allotted_days = models.DecimalField(max_digits=3, decimal_places=1, default=30.0)
     transferred_days = models.IntegerField(default=0)
+    remote_work_days = models.IntegerField(default=30)
     # TODO: make sure this is the year in which
     year = models.IntegerField(default=datetime.datetime.now().year, editable=True)
 
@@ -63,7 +64,7 @@ class AvailableDays(models.Model):
 
 
 class Request(models.Model):
-    TYPES = ((1, "vacation"), (2, "transfer"), (3, "cancel"))
+    TYPES = ((1, "vacation"), (2, "transfer"), (3, "cancel"), (4, "remote_work"))
     STATUS_OPTIONS = ((1, "pending"), (2, "approved"), (3, "rejected"))
 
     employee = models.ForeignKey(
@@ -76,3 +77,9 @@ class Request(models.Model):
     request_status = models.IntegerField(choices=STATUS_OPTIONS, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+
+class RemoteDay(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField()
+    description = models.TextField(max_length=200, null=True, blank=True)
