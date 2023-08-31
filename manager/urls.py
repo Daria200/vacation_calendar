9c295e2fcd5688757b0a392ec0c2739f3c943abb
Daria Neznaeva <darianeznaeva@gmail.com>
2023-08-31 12:46:50 +0200

    removing delete, vacation, and transfer requests views

--- manager/urls.py
+++ manager/urls.py
@@ -4,15 +4,6 @@ from . import views
 
 urlpatterns = [
     path("requests", views.requests, name="requests"),
-    path("vacation_requests", views.vacation_requests, name="vacation_requests"),
-    path(
-        "transfer_days_requests",
-        views.transfer_days_requests,
-        name="transfer_days_requests",
-    ),
-    path(
-        "cancel_days_requests", views.cancel_days_requests, name="cancel_days_requests"
-    ),
     path("hr_view", views.hr_view, name="hr_view"),
     path(
         "manager_view_monthly", views.manager_view_monthly, name="manager_view_monthly"
